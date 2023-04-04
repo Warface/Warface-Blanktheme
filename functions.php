f2ffd69b83792766e455a651d9031055612fe8e1
Warface <carl.spooner1982@gmail.com>
2023-04-04 14:42:00 -0400

    Update functions.php
    Added a square image

--- functions.php
+++ functions.php
@@ -49,6 +49,7 @@ if (function_exists('add_theme_support')){
     add_image_size('medium', 250, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
     add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+    add_image_size('square', 800, 800, true);
 
 
     // Enables post and comment RSS feed links to head
