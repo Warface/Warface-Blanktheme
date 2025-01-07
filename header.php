dcb5d238c55e7f8f54666608a5137a7d48ee80c0
Warface <carl.spooner1982@gmail.com>
2025-01-07 10:00:48 -0500

    Update header.php
    Remove that stupid class serving nothing at all but problems with AOS

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php wp_title(''); ?></title>
